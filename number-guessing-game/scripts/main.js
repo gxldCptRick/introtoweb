b9a1a41bdd1957a98e34bbe9fa19f692f1b475b5
jefe <andreshcar@live.com>
2018-01-18 15:41:27 -0700

    adding the functionality of java script to the form

---
+++ number-guessing-game/scripts/main.js
@@ -0,0 +1,4 @@
+var guessForm = document.getElementById("guessForm");
+var lastGuessText = document.getElementById("last-guess-text");
+var helperText = document.getElementById("helper-text");
+var guessButton = guessForm.guessButton;
